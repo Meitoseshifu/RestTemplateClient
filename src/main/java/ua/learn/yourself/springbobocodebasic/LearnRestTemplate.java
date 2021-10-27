c3a4a6e26fb2195645b954eb8fe0bfe3201f1bce
Andriy Paliychuk <wantstraddle@gmail.com>
2021-10-27 20:22:07 +0300

    happy learning

--- src/main/java/ua/learn/yourself/springbobocodebasic/LearnRestTemplate.java
+++ src/main/java/ua/learn/yourself/springbobocodebasic/LearnRestTemplate.java
@@ -1,11 +1,5 @@
 package ua.learn.yourself.springbobocodebasic;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
-
 public class LearnRestTemplate {
 
     public static void main(String[] args) {
@@ -15,37 +9,33 @@ public class LearnRestTemplate {
         postRequestEntityAndGetResponseEntity();
     }
 
+    /**
+     * use RestTemplate
+     * get messages from "http://localhost:8889/messages" endpoint
+     */
     static void getMessages() {
-        RestTemplate restTemplate = new RestTemplate();
-        String s = restTemplate.getForObject("http://localhost:8889/messages", String.class); //4oxo ne povertae message
-        System.out.println(s);
+
     }
 
+    /**
+     * post message to "http://localhost:8889/messages" and get Message as response
+     */
     static void postMessage() {
-        RestTemplate restTemplate = new RestTemplate();
-        HttpEntity<Message> httpEntity = new HttpEntity<>(new Message(2L, "popostuv", "9"));
-        Message message = restTemplate.postForObject("http://localhost:8889/messages", httpEntity, Message.class);
-        System.out.println(message);
+
     }
 
+    /**
+     * post RequestEntity to http://localhost:8889/messages endpoint
+     * in RequestEntity should be header X-Mood and message
+     */
     static void postRequestEntityWithHeader() {
-        RestTemplate restTemplate = new RestTemplate();
-        RequestEntity<Message> body = RequestEntity.post("http://localhost:8889/messages")
-                .header("X-Mood", "4erepashka")
-                .body(new Message(3L, "I'm in request entity baby", "pyzdata 4erepashka"));
-        Message message = restTemplate.postForObject("http://localhost:8889/messages", body, Message.class);
-        System.out.println(message);
+
     }
 
+    /**
+     * post RequestEntity with header and message and get ResponseEntity as response
+     */
     static void postRequestEntityAndGetResponseEntity() {
-        RestTemplate restTemplate = new RestTemplate();
-        RequestEntity<Message> body = RequestEntity.post("http://localhost:8889/messages")
-                .header("X-Mood", "depressed")
-                .body(new Message(5L, "why I dont have any questions? Am I stupid?", "master lomaster"));
-
-        ResponseEntity<Message> exchange = restTemplate
-                .exchange("http://localhost:8889/messages", HttpMethod.POST, body, Message.class);
-        System.out.println(exchange);
 
     }
 }
